84338fdd0e7b7715a42ae1068c3cddb9144e86c3
pavel <pavel_solovev21@mail.ru>
2022-11-14 05:57:12 +0300

    feat: add accept income handler

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -9,6 +9,7 @@ type (
 	UserRp interface {
 		CheckUserBalanceExistence(context.Context, uuid.UUID) (bool, error)
 		CheckUserReserveExistence(context.Context, uuid.UUID) (bool, error)
+		CheckRequiredReserveExistence(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) (bool, error)
 		CreateNewBalance(context.Context, uuid.UUID, uint64) error
 		CheckEnoughMoneyBalance(context.Context, uuid.UUID, uint64) (bool, error)
 		CreateNewReserve(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) error
@@ -16,7 +17,7 @@ type (
 		GetBalance(context.Context, uuid.UUID) (int64, error)
 		GetReserve(context.Context, uuid.UUID) ([]int64, error)
 		ReserveMoney(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) error
-		AcceptIncome(context.Context, uuid.UUID, uuid.UUID, int64) error
+		AcceptIncome(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) error
 	}
 
 	UserContract interface {
@@ -24,6 +25,6 @@ type (
 		GetBalance(context.Context, uuid.UUID) (int64, error)
 		ReserveMoney(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) error
 		GetReserve(context.Context, uuid.UUID) ([]int64, error)
-		AcceptIncome(context.Context, uuid.UUID, uuid.UUID, int64) error
+		AcceptIncome(context.Context, uuid.UUID, uuid.UUID, uuid.UUID, uint64) error
 	}
 )
